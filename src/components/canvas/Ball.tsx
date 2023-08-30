24f18f79f6165bbbee00f48eef5c92a468b45bcc
Andrés David Betancourt Santana <65095693+AndresBetancourt-Dev@users.noreply.github.com>
2023-08-29 23:25:00 -0500

    🎱 Added Balls with Tech Tool Logo

--- src/components/canvas/Ball.tsx
+++ src/components/canvas/Ball.tsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { FC, Suspense } from "react";
+import { Canvas, useLoader } from "@react-three/fiber";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
 
-const Ball = () => {
-  return (
-    <div>Ball</div>
-  )
+import CanvasLoader from "../Loader";
+import { StaticImageData } from "next/image";
+
+interface BallProps {
+  icon: StaticImageData;
 }
 
-export default Ball
+const Ball: FC<BallProps> = (props) => {
+  const [decal] = useTexture([props.icon.src]);
+
+  return (
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
+          color="#fff8eb"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          scale={1}
+          map={decal}
+        />
+      </mesh>
+    </Float>
+  );
+};
+
+interface BallCanvasProps {
+  icon: StaticImageData;
+}
+
+const BallCanvas: FC<BallCanvasProps> = ({ icon }) => {
+  return (
+    <Canvas
+      frameloop="demand"
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} />
+        <Ball icon={icon} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default BallCanvas;
